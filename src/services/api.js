5364c11fe1402b5a6814c8376eb36b426fa896ba
charuwanTT <charuwanteam26@gmail.com.com>
2026-03-17 16:01:16 +0700

    hardcode backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 // src/services/api.js
-const BASE = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api`
-  : '/api'
+const BASE = 'https://floodwatch-backend-production.up.railway.app/api'
 
 export async function getAllDistricts() {
   try {
